b0418159de3f22353323c99dc0e0bcb51fad6f47
SantiJaime <santij03@gmail.com>
2023-09-20 22:24:23 -0300

    Añado página y ruta de register, validaciones con yup y modifico navbar

--- src/routes/RoutesView.jsx
+++ src/routes/RoutesView.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import ErrorPage from '../pages/ErrorPage'
+import RegisterPage from '../pages/RegisterPage'
 
 const RoutesView = () => {
   return (
     <Routes>
+        <Route path='/register' element={<RegisterPage/>}/>
         <Route path='/' element={<HomePage/>}/>
         <Route path='*' element={<ErrorPage/>}/>
     </Routes>
---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,8 @@
+import * as yup from 'yup'
+
+export const errorRegisterSchema = yup.object().shape({
+    email: yup.string().email("Formato Email inválido").required("Campo correo electrónico obligatorio"),
+    name: yup.string().required("Campo nombre y apellido obligatorio"),
+    pass: yup.string().required("Campo contraseña obligatorio").min(8, "La contraseña debe ser de al menos 8 caracteres"),
+    repeatPass: yup.string().required("Campo repetir contraseña obligatorio")
+})
