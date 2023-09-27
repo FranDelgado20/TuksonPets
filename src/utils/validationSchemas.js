886fd9f892c9f7ced3feece31a6d73a559b63d8f
SantiJaime <santij03@gmail.com>
2023-09-27 12:46:41 -0300

    Añado tel a register comp, modifico esquema de validacion

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -4,7 +4,8 @@ export const errorRegisterSchema = yup.object().shape({
     email: yup.string().email("Formato Email inválido").required("Campo correo electrónico obligatorio"),
     name: yup.string().required("Campo nombre y apellido obligatorio"),
     pass: yup.string().required("Campo contraseña obligatorio").min(8, "La contraseña debe ser de al menos 8 caracteres"),
-    repeatPass: yup.string().required("Campo repetir contraseña obligatorio").min(8, "La contraseña debe ser de al menos 8 caracteres")
+    repeatPass: yup.string().required("Campo repetir contraseña obligatorio").min(8, "La contraseña debe ser de al menos 8 caracteres"),
+    tel: yup.number().required("Campo número telefónico obligatiorio").min(10, "Formato inválido. El número debe tener 10 caracteres")
 })
 
 export const errorLoginSchema = yup.object().shape({
